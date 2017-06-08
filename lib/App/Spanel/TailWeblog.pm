9fbbaee5dbb889822464b025de2450be969516db
perlancar (on pc-office) <perlancar@gmail.com>
2017-06-08 14:10:30 +0700

    Initial commit

---
+++ lib/App/Spanel/TailWeblog.pm
@@ -0,0 +1,13 @@
+package App::Spanel::TailWeblog;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Tail -f spanel HTTP and/or HTTPS access log
+
+=head1 SYNOPSIS
+
+See the included L<spanel-tail-weblog> script.
+
+=cut
